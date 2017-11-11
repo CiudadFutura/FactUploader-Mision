904bb23189c517592708e2aade449c75bba94a83
azacchino@users.noreply.github.com <azacchino@users.noreply.github.com>
2017-11-11 17:02:49 -0300

    Init

--- Files2Server/Program.cs
+++ Files2Server/Program.cs
@@ -3,13 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WinSCP;
 
 namespace Files2Server
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+
+            String certPath = System.IO.Directory.GetCurrentDirectory() + "\\pk.ppk";
+            String cfgPath = System.IO.Directory.GetCurrentDirectory() + "\\lastdate.cfg";
+            try
+            {
+                if (!System.IO.File.Exists(certPath)) {
+                    Console.WriteLine("Falta la clave privada");
+                    return;
+                }
+
+                DateTime dt = DateTime.Now;
+                String hoy = "";
+                if (System.IO.File.Exists(cfgPath))
+                {
+                    hoy = DateTime.TryParse(System.IO.File.ReadAllText(cfgPath), out dt) ? dt.ToString("_yyMMdd") : "";
+                }
+
+                // Setup session options
+                SessionOptions sessionOptions = new SessionOptions
+                {
+                    Protocol = Protocol.Scp,
+                    HostName = config.getInstance().HostName,
+                    UserName = config.getInstance().UserName,
+                    PrivateKeyPassphrase = config.getInstance().PrivateKeyPassphrase,
+                    SshPrivateKeyPath = System.IO.Directory.GetCurrentDirectory() + "\\pk.ppk",
+                    //TlsClientCertificatePath = "pk.ppk",
+                    //SshHostKeyFingerprint = "" //ssh-rsa 2048 xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx
+                    GiveUpSecurityAndAcceptAnySshHostKey = true,
+                };
+
+                using (Session session = new Session())
+                {
+                    session.FileTransferred += Session_FileTransferred;
+                    // Connect
+                    session.Open(sessionOptions);
+                    Console.WriteLine("Conexion abierta");
+                    // Upload files
+                    TransferOptions transferOptions = new TransferOptions();
+                    transferOptions.TransferMode = TransferMode.Binary;
+                    transferOptions.OverwriteMode = OverwriteMode.Overwrite;
+                    Console.WriteLine("Subiendo archivos.");
+                    TransferOperationResult transferResult;
+
+                    do
+                    {
+                        transferResult = session.PutFiles(config.getInstance().OrgPath + "*" + hoy + "*.pdf", config.getInstance().DestPath, false, transferOptions);
+                        dt = dt.AddDays(1);
+                        hoy = dt.ToString("_yyMMdd");
+                    } while (dt < DateTime.Now);
+                    
+                    // Throw on any error
+                    transferResult.Check();
+
+                    // Print results
+                    foreach (TransferEventArgs transfer in transferResult.Transfers)
+                    {
+                        Console.WriteLine("Subir {0} terminado", transfer.FileName);
+                    }
+                }
+                System.IO.File.WriteAllText(cfgPath, DateTime.Now.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e);
+            }
+
+#if DEBUG
+            Console.WriteLine("Fin");
+            Console.ReadLine();
+#endif
+        }
+
+        private static void Session_FileTransferred(object sender, TransferEventArgs e)
+        {
+            Console.WriteLine("Archivo {0} transferido: {1}", e.FileName, e.Error == null ? "OK" : e.Error.Message);
         }
     }
 }
---
+++ Files2Server/config.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace Files2Server
+{
+    class config
+    {
+        [Serializable]
+        public class storage
+        {
+            public string PrivateKeyPassphrase = "pass";
+            public string HostName = "domain";
+            public string UserName = "usr";
+            public string OrgPath = "E:\\hola";
+            public string DestPath = "/home/hola";
+        }
+
+        static config INSTANCE;
+
+        storage st;
+        String cfgPath = System.IO.Directory.GetCurrentDirectory() + "\\cfg.xml";
+
+        private config() {
+            
+            IFormatter formatter = new SoapFormatter();
+            Stream stream;
+            if (System.IO.File.Exists(cfgPath))
+            {
+                stream = new FileStream(cfgPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+                st = (storage)formatter.Deserialize(stream);
+            }
+            else {
+                st = new storage();
+                stream = new FileStream(cfgPath, FileMode.Create, FileAccess.Write, FileShare.None);
+                formatter.Serialize(stream, st);
+            }
+            stream.Close();
+
+        }
+
+        public static config getInstance() {
+            return INSTANCE != null ? INSTANCE : INSTANCE = new config();
+        }
+
+        public string PrivateKeyPassphrase
+        {
+            get
+            {
+                return st.PrivateKeyPassphrase;
+            }
+        }
+        public string HostName {
+            get
+            {
+                return st.HostName;
+            }
+        }
+        public string UserName {
+            get
+            {
+                return st.UserName;
+            }
+        }
+
+        public string OrgPath
+        {
+            get
+            {
+                return st.OrgPath;
+            }
+        }
+
+        public string DestPath
+        {
+            get
+            {
+                return st.DestPath;
+            }
+        }
+    }
+}
